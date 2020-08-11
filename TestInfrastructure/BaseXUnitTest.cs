11b31c4cb1b7c2e9ff7208aac15a11bed3338ff4
AgPawe <69428041+AgPawe@users.noreply.github.com>
2020-08-11 22:12:25 +0200

    Add files via upload

---
+++ TestInfrastructure/BaseXUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using RyłkoWeb_TestFramework.Rylko_Pages;
+using RyłkoWeb_TestFramework.Rylko_Pages.Rylko_MainPage;
+using RyłkoWeb_TestFramework.Rylko_Pages.Rylko_SearchPage;
+
+namespace RyłkoWeb_TestFramework.TestInfrastructure
+{
+    public abstract class BaseXUnitTest : BaseXUnitContext
+    {
+        protected BaseXUnitTest()
+        {
+            RylkoMainPage = new RylkoMainPage();
+            LoginPage = new LoginPage();
+            SearchPage = new SearchPage();
+        }
+        protected RylkoMainPage RylkoMainPage;
+        protected LoginPage LoginPage;
+        protected SearchPage SearchPage;
+        
+    }
+}
---
+++ TestInfrastructure/XUnit/WebDriverWrapper.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace RyłkoWeb_TestFramework.TestInfrastructure.XUnit
+{
+    public class WebDriverWrapper
+    {
+        static WebDriverInitialize WebDriverInitialize = new WebDriverInitialize();
+
+        public static IWebDriver Driver()
+        {
+            return WebDriverInitialize.driver;
+        }
+
+        public static IWebDriver Page => Driver();
+    }
+}
