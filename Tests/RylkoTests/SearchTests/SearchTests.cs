47d06632e11b03ece6bd00ddbc8a797eab80973a
AgPawe <69428041+AgPawe@users.noreply.github.com>
2020-08-11 22:15:18 +0200

    Add files via upload

---
+++ Tests/RylkoTests/SearchTests/SearchTests.cs
@@ -0,0 +1,19 @@
+﻿using RyłkoWeb_TestFramework.TestInfrastructure;
+using Xunit;
+
+namespace Tests.RylkoTests.SearchTests
+{
+    public class SearchTests : BaseXUnitTest
+    {
+        [Fact]
+        public void SearchProductTest()
+        {
+            SearchPage.SearchProduct(availableProduct);
+            SearchPage.ClearSearch();
+            SearchPage.SearchProduct(unavailableProduct, available: false);
+        }
+
+        private const string availableProduct = "kozaki";
+        private const string unavailableProduct = "okulary";
+    }
+}
