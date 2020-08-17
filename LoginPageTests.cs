b5edca2fb078e591f9492c37c1d18295d3576819
AgPawe <69428041+AgPawe@users.noreply.github.com>
2020-08-17 22:05:03 +0200

    Add files via upload

---
+++ LoginPageTests.cs
@@ -0,0 +1,32 @@
+﻿using RyłkoWeb_TestFramework.TestInfrastructure;
+using Xunit;
+
+namespace Tests.RylkoTests.LoginTests
+{
+    public class RegistrationTest : BaseXUnitTest
+    {
+        [Fact]
+        public void SuccessfulRegistrationUserTest()
+        {
+            LoginPage.NavigateToLoginPage();
+            LoginPage.RegisterUser(unregisteredEmail, checkAllCheckboxes: false, registrationSuccessful: true);
+        }
+
+        [Fact]
+        public void RegistrationfailureTest()
+        {
+            LoginPage.NavigateToLoginPage();
+            LoginPage.RegisterUser(registeredEmail, checkAllCheckboxes: false, registrationSuccessful: false);
+        }
+
+        [Fact]
+        public void PasswordRecoveryTest()
+        {
+            LoginPage.NavigateToLoginPage();
+            LoginPage.PasswordRecovery(registeredEmail);
+        }
+
+        private const string registeredEmail = "agato.pawelczak @gmail.com";
+        private const string unregisteredEmail = "rylkotest@gmail.com";
+    }
+}
---
+++ RylkoMainPageTest.cs
@@ -0,0 +1,22 @@
+﻿using RyłkoWeb_TestFramework.Rylko_Pages;
+using RyłkoWeb_TestFramework.Rylko_Pages.Enums;
+using RyłkoWeb_TestFramework.TestInfrastructure;
+using RyłkoWeb_TestFramework.TestInfrastructure.Enums;
+using Xunit;
+
+namespace RyłkoWeb_TestFramework.Rylko_Tests
+{
+    public partial class MainPage : BaseXUnitTest
+    {
+        [Fact]
+        public void RylkoMainPageTest()
+        {
+            RylkoMainPage.GoToCategory(TopPanelCategories.Bags);
+            RylkoMainPage.SelectLanguage(Languages.Russian);
+            RylkoMainPage.SelectLanguage(Languages.Polish);
+            RylkoMainPage.NavigateToLeftCornerOption(MainPageTopPanelOptions.ZnajdzNas);
+            LoginPage.NavigateToLoginPage();
+
+        }
+    }
+}
