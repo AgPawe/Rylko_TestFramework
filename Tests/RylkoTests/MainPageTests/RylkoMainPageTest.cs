e785ca30d6e1e466affc41739b04c60ef9572265
AgPawe <69428041+AgPawe@users.noreply.github.com>
2020-08-17 22:13:48 +0200

    Add files via upload

---
+++ Tests/RylkoTests/MainPageTests/RylkoMainPageTest.cs
@@ -0,0 +1,21 @@
+﻿using RyłkoWeb_TestFramework.Rylko_Pages.Enums;
+using RyłkoWeb_TestFramework.TestInfrastructure;
+using RyłkoWeb_TestFramework.TestInfrastructure.Enums;
+using Xunit;
+
+namespace RyłkoWeb_TestFramework.Rylko_Tests
+{
+    public partial class MainPage : BaseXUnitTest
+    {
+        [Fact]
+        public void RylkoMainPageTest()
+        {
+            RylkoMainPage.NavigateToTopPanelShoppingOption(MainPageTopPanelShoppingOption.Bags);
+            RylkoMainPage.SelectLanguage(Languages.Russian);
+            RylkoMainPage.SelectLanguage(Languages.Polish);
+            RylkoMainPage.NavigateToTopPanelLogoOption(MainPageTopPanelLogoOptions.ZnajdzNasLink);
+            LoginPage.NavigateToLoginPage();
+
+        }
+    }
+}
